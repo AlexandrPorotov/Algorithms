ea5a6eb932da475e1d3b9dc28cf475ab2f681808
alexandr.porotov@yandex.ru <xDv_3t_o_pr>
2022-09-19 15:28:26 +0300

    LessonTwo add log4j and slf4j

--- src/main/java/LessonTwo/Solutions/RleLastSolution.java
+++ src/main/java/LessonTwo/Solutions/RleLastSolution.java
@@ -1,17 +1,24 @@
 package LessonTwo.Solutions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
 public class RleLastSolution {
 
+    private static final Logger logger = LoggerFactory.getLogger(RleLastSolution.class);
+
     public static void main(String[] args) {
-
+        logger.info("Program started;");
         RleLastSolution.go("AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB");
-
+        logger.info("Program end;");
     }
 
     public static void go(String s){
-
+        logger.info("RleLastSolution.go() get param - " + s);
         if(s.equals("")){
-            System.out.println("Error, input string should not be empty.");
+            logger.error("Error, input string should not be empty.");
         }
 
         System.out.println(rleAndCount(s.split("")));
@@ -19,6 +26,8 @@ public class RleLastSolution {
     }
 
     public static String rleAndCount(String[] chars){
+
+        logger.info("RleLastSolution.rleAndCount get started with param - " + Arrays.toString(chars));
 
         StringBuilder ans = new StringBuilder();
         String lastSym = chars[0];
@@ -33,12 +42,13 @@ public class RleLastSolution {
         }
 
         ans.append(pack(lastSym, chars.length - lastPos));
-
+        logger.info("RleLastSolution.rleAndCount get stop - ans = " + ans.toString());
         return ans.toString();
 
     }
 
     public static String pack(String s, int cnt){
+        logger.info("RleLastSolution.pack() get started with param - " + s + " - " +  cnt);
         return cnt > 1 ? s+cnt : s;
     }
 
