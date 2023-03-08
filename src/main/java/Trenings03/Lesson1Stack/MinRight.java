e4f982988daa2f57b6ab4db30b2f045dd6602753
alexandr.porotov@yandex.ru <xDv_3t_o_pr>
2023-03-08 19:43:30 +0100

    search for all minimums on the right

---
+++ src/main/java/Trenings03/Lesson1Stack/MinRight.java
@@ -0,0 +1,52 @@
+package Trenings03.Lesson1Stack;
+
+//найти наименьшее справа для каждого элемента
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class MinRight {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{7, 2, 4, 5, 3, 2, 5, 1, 5, 4};
+
+        System.out.println(Arrays.toString(findAllMin(arr)));
+
+    }
+
+    public static int[] findAllMin(int[] arr) {
+
+        int[] answer = new int[arr.length];
+
+        Stack<Integer> stackIndex = new Stack<>();
+        Stack<Integer> stackValue = new Stack<>();
+
+        stackValue.push(arr[0]);
+        stackIndex.push(0);
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > stackValue.peek()){
+                stackValue.push(arr[i]);
+                stackIndex.push(i);
+            } else if (arr[i] < stackValue.peek()) {
+                while (!stackValue.empty() && arr[i] < stackValue.peek()) {
+                    answer[stackIndex.peek()] = i;
+                    stackIndex.pop();
+                    stackValue.pop();
+                }
+                stackIndex.push(i);
+                stackValue.push(arr[i]);
+            }
+        }
+
+        while (!stackIndex.empty()){
+            answer[stackIndex.peek()] = arr.length;
+            stackIndex.pop();
+        }
+
+
+        return answer;
+    }
+
+}
