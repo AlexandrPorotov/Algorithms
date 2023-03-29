89715edef87d04f961202490b2bb58d3c441112f
alexandr.porotov@yandex.ru <xDv_3t_o_pr>
2023-03-29 22:17:58 +0200

    lesson 4 ex4

---
+++ src/main/java/Trenings01/Lesson4/Example4.java
@@ -0,0 +1,33 @@
+package Trenings01.Lesson4;
+
+
+import java.util.*;
+
+//Сгруппировать слова по общим буквам
+//eat, tea, tan, ate, nat, bat
+public class Example4 {
+
+    public static void main(String[] args) {
+        solution("eat, tea, tan, ate, nat, bat");
+    }
+    private static void solution(String input){
+
+        String[] words = input.split(", ");
+        HashMap<String, ArrayList<String>> groups = new HashMap<>();
+        //так же можно считать через сортировку подсчетом в качестве оптимизации
+        for(String s : words){
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortWord = new String(chars);
+            if (!groups.containsKey(sortWord)) {
+                groups.put(sortWord, new ArrayList<>());
+            }
+            groups.get(sortWord).add(s);
+        }
+
+        for(String s : groups.keySet()){
+            System.out.println(s);
+            System.out.println(groups.get(s));
+        }
+    }
+}
