6372ee30b730bb98c152949c67be8c13f2d40851
aleks <alexandr.porotov@yandex.ru>
2023-06-17 19:30:24 +0200

    countSeniors

--- src/test/java/LeetCode/NumberOfSeniorCitizensTest.java
+++ src/test/java/LeetCode/NumberOfSeniorCitizensTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 //The phone numbers and seat numbers of the passengers are distinct.
 class NumberOfSeniorCitizensTest {
 
+
     private static NumberOfSeniorCitizens numberOfSeniorCitizens;
 
     @BeforeAll
