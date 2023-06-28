ce2e9c0df939add923611582c102c1b5f7af09c9
aleks <alexandr.porotov@yandex.ru>
2023-06-28 21:38:09 +0200

    FindTheIndexOfTheFirstOccurrenceInAString

---
+++ src/main/java/LeetCode/EasyLevel/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,12 @@
+package LeetCode.EasyLevel;
+
+
+//Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
