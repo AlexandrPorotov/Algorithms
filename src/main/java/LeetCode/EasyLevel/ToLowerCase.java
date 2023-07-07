b20e95ad4521401a84dcc9db8a9834aea49fe62f
aleks <alexandr.porotov@yandex.ru>
2023-07-07 03:14:04 +0200

    ToLowerCase

---
+++ src/main/java/LeetCode/EasyLevel/ToLowerCase.java
@@ -0,0 +1,10 @@
+package LeetCode.EasyLevel;
+
+//https://leetcode.com/problems/to-lower-case/description/
+public class ToLowerCase {
+
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+}
